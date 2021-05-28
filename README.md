0b686f77d9c1d3e3f3b95446b673e3ad6b7ebd36
Mauricio Heredia <mheredia@makingsense.com>
2021-05-28 10:37:04 -0300

    Add Readme

---
+++ README.md
@@ -0,0 +1,22 @@
+#The Fork android application
+
+### Branch strategy
+
+### Dependencies used: 
+| Name  |Use   |License   |
+|---|---|---|
+|
+|
+| Constraint Layout(part of Android Jetpack)  | UI component  
+| Ktor  | REST Client  |
+| kotlinx-serialization-json | for Json serializer 
+| Dagger Hilt  | Dependency Injection 
+| Timber For Log
+| Coroutines  | Concurrency components
+| Clean Architecture 
+| MVI - MVVM 
+|  
+| Espresso  | Testing  
+| Mockk  | Testing  | 
+
+
---
+++ app/src/main/java/trip/thefork/ui/features/restaurant/ImageAdapter.kt
@@ -0,0 +1,27 @@
+package trip.thefork.ui.features.restaurant
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import trip.thefork.databinding.ImageItemBinding
+
+class ImageAdapter(private val list: List<String>) : RecyclerView.Adapter<ImageVH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageVH = ImageVH(
+        ImageItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    override fun onBindViewHolder(holder: ImageVH, position: Int) {
+        Picasso.with(holder.itemView.context).load(list[position]).into(holder.binding.image)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+
+}
+
+class ImageVH(val binding: ImageItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
